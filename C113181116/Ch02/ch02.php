7ac0b5fc356791e5faaf2215bc86724d8d3ee618
zhiyuan1146 <C113181116@nkust.edu.tw>
2026-09-24 11:52:57 +0800

    Add files via upload

---
+++ C113181116/Ch02/ch02.php
@@ -0,0 +1,14 @@
+<?php
+$begin=12;
+$end=18;
+for($i=$begin; $i < $end; $i+=3){
+?>
+
+<p style="font-size: <?php echo $i?> pt">
+My first PHP program, My ID: C113181116
+
+<?php
+print "</p>";
+}
+?>
+keep
---
+++ C113181116/Ch02/hello.php
@@ -0,0 +1,6 @@
+<?php
+echo "zhiyuan";
+echo "<P>";
+echo "C113181116";
+
+phpinfo();
